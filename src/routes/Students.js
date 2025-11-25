348eb38e75dc7fb90b3f03f4cdfc52d62346a947
Wesley Bezerra <wesleybezerra770@gmail.com>
2025-11-25 16:41:04 -0300

    ...

--- src/routes/Students.js
+++ src/routes/Students.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const controllersStudents = require('../controllers/Students');
-const { authMiddleware,definePasswordMiddleware } = require('../middleware/Students');
+const { authMiddleware, definePasswordMiddleware } = require('../middleware/Students');
 
 router.post('/', controllersStudents.createStudent);
 router.get('/', controllersStudents.getStudentsAll);
 router.get('/getByMatricula', controllersStudents.getStudentByMatricula)
 router.post('/login', controllersStudents.login);
-router.post('/definir-senha', definePasswordMiddleware ,controllersStudents.definePassword);
-router.get('/me', authMiddleware ,controllersStudents.me);
+router.post('/definir-senha', definePasswordMiddleware, controllersStudents.definePassword);
+router.get('/me', authMiddleware, controllersStudents.me);
+router.put('/', controllersStudents.updateStudent);
 
 
 
