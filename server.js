b1359c9d4765e5c54bb7ccfd2d3a2ab2b4985313
Wesley Bezerra <wesleybezerra770@gmail.com>
2025-12-01 16:58:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const disciplinesRoutes = require("./src/routes/Discipline");
 app.use(express.json());
 
 app.use(cors({
-    origin: ["*", "http://localhost:5173", "https://dl3w-6qpm.vercel.app/"],
+    origin: ["*", "http://localhost:5173", "https://dl3w-6qpm.vercel.app"],
 }));
 
 
